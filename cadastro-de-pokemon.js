c37fc81f2942eb9034a22064587c75ab4608300a
Marcus Serafim <95192746+SerafimL147@users.noreply.github.com>
2023-09-24 01:43:04 -0300

    att

---
+++ cadastro-de-pokemon.js
@@ -0,0 +1,31 @@
+//Pokemons:     SEXO  -  NIVEL  -  HP
+
+//Poochyena (M/2/13)  (F/3/15)
+
+//Zigzagoon (F/2/13)
+
+//Dragonite (M/2/25) (F/5/24) (F/5/24)
+
+//Murmple (M/2/7)
+
+
+let mensagemPokemon = ", Cadastrado com Sucesso"
+let propriedadesPokemon = [
+  ["Poochyena", " Masculino"," Nivel 2", " HP 13"],
+  ["Poochyena", " Feminino"," Nivel 3", " HP 15"],
+  ["Zigzagoon", " Feminino"," Nivel 2", " HP 13"],
+  ["Dragonite", " Masculino"," Nivel 2", " HP 25"],
+  ["Dragonite", " Feminino"," Nivel 5", " HP 24"],
+  ["Dragonite", " Feminino"," Nivel 5", " HP 24"],
+  ["Murmple", " Masculino"," Nivel 2", " HP 7"]
+
+]
+
+console.log("Pokemon " + propriedadesPokemon[0] + mensagemPokemon)
+console.log("Pokemon " + propriedadesPokemon[1] + mensagemPokemon)
+console.log("Pokemon " + propriedadesPokemon[2] + mensagemPokemon)
+console.log("Pokemon " + propriedadesPokemon[3] + mensagemPokemon)
+console.log("Pokemon " + propriedadesPokemon[4] + mensagemPokemon)
+console.log("Pokemon " + propriedadesPokemon[5] + mensagemPokemon)
+console.log("Pokemon " + propriedadesPokemon[6] + mensagemPokemon)
+
---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = "Marcuss"
+
+if(nome === "Marcus"){
+    console.log("Vc passou")
+}
